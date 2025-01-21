93ee180e6bd2bcdefbcd77f2166feaf53ab9d93f
Yiheng Shu <shuyiheng29@163.com>
2025-01-21 09:14:16 +0800

    Update oracle_ensemble.py

--- src/analysis/oracle_ensemble.py
+++ src/analysis/oracle_ensemble.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 from collections import defaultdict
 
 
@@ -28,6 +29,8 @@ if __name__ == '__main__':
     data1 = json.load(open(args.log1))
     data2 = json.load(open(args.log2))
 
+    assert len(data1) == len(data2), f'Two logs have different number of samples: {len(data1)} vs {len(data2)}'
+
     recall_k = 5
     ensemble = []
     win = []
@@ -37,7 +40,7 @@ if __name__ == '__main__':
         sample1 = data1[i]
         sample2 = data2[i]
         question_key = 'question' if 'question' in sample1 else 'query'
-        assert sample1[question_key] == sample2[question_key]
+        assert sample1[question_key] == sample2[question_key], f'Two queries at {i} are not the same: {sample1[question_key]}\t{sample2[question_key]}'
 
         if 'recall' in sample1 and 'recall' in sample2:
             metric1 = round(sample1['recall'][str(recall_k)], 3)
@@ -49,16 +52,25 @@ if __name__ == '__main__':
         if metric1 > metric2:
             sample1['metric1'] = metric1
             sample1['metric2'] = metric2
+            sample1['retrieved1'] = sample1['retrieved']
+            sample1['retrieved2'] = sample2['retrieved']
+            del sample1['retrieved']
             ensemble.append(sample1)
             win.append(sample1)
         elif metric1 < metric2:
             sample2['metric1'] = metric1
             sample2['metric2'] = metric2
+            sample2['retrieved1'] = sample1['retrieved']
+            sample2['retrieved2'] = sample2['retrieved']
+            del sample2['retrieved']
             ensemble.append(sample2)
             loss.append(sample2)
         else:
             sample1['metric1'] = metric1
             sample1['metric2'] = metric2
+            sample1['retrieved1'] = sample1['retrieved']
+            sample2['retrieved2'] = sample2['retrieved']
+            del sample1['retrieved']
             ensemble.append(sample1)
             tie.append(sample1)
 
@@ -90,9 +102,7 @@ if __name__ == '__main__':
     for key in metrics:
         metrics[key] /= len(ensemble)
         print(f'{key}: {round(metrics[key], 3)}')
-    print(f'num_select1: {len(win) / len(ensemble)}')
-    print(f'num_select2: {len(loss) / len(ensemble)}')
-    print(f'num_tie: {len(tie) / len(ensemble)}')
+    print(f'select 1: {len(win) * 100 / len(ensemble)}%, select 2: {len(loss) * 100 / len(ensemble)}%, tie: {len(tie) * 100 / len(ensemble)}%')
     print(f'total: {len(ensemble)}')
     print()
 
@@ -104,3 +114,11 @@ if __name__ == '__main__':
     statistics(win, metrics_win, 'win')
     statistics(loss, metrics_loss, 'loss')
     statistics(tie, metrics_tie, 'tie')
+
+    os.makedirs('output/compare', exist_ok=True)
+    with open('output/compare/win.json', 'w') as f:
+        json.dump(win, f, indent=2)
+    with open('output/compare/loss.json', 'w') as f:
+        json.dump(loss, f, indent=2)
+    with open('output/compare/tie.json', 'w') as f:
+        json.dump(tie, f, indent=2)
